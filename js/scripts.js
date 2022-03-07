68696bf23a59c8488c2709036e483594ba94efa0
marinanadj <marinanadj7@yahoo.com>
2022-03-07 11:15:08 +0100

    Loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,37 @@
-let pokemonList = [{
-    name: 'Bulbasaur',
-    height: 0.7,
-    weight: 6.9,
-    type: ['grass', 'poison']
-  },
-  
-  {
-    name: 'Charmilion',
-    height: 1.1,
-    weight: 19,
-    type: ['fire']
-  },
-  
-  {
-    name: 'Dratini',
-    height: 1.8,
-    weight: 3.3,
-    type: ['dragpn']
-  },
-  
-  {
-    name: 'Rayquaza',
-    height: 7,
-    weight: 206.5,
-    type: ['dragon']
-  }
-  ];
+
+  let pokemonList = [{
+
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', 'poison']
+},
+
+{
+  name: 'Charmilion',
+  height: 1.1,
+  weight: 19,
+  type: ['fire']
+},
+
+{
+  name: 'Dratini',
+  height: 1.8,
+  weight: 3.3,
+  type: ['dragon']
+},
+
+{
+  name: 'Rayquaza',
+  height: 7,
+  weight: 206.5,
+  type: ['dragon']
+}];
+
+for (let i=0; i < pokemonList.length; i++){
+  if(pokemonList[i].height > 3){
+    document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + ")" + "Wow it`s a big pokemon!!! <br>")
+  }else {
+  document.write(pokemonList[i].name + "( height : " + pokemonList[i].height + ")<br>")
+}
+}
